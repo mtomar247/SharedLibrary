18893e2a6af6774f4d3e78a608b603b847199b46
mtomar247 <124338224+mtomar247@users.noreply.github.com>
2023-02-27 17:19:07 +0530

    Update lib.groovy

--- vars/lib.groovy
+++ vars/lib.groovy
@@ -2,6 +2,7 @@ def call(String stageName){
   if ("$stageName"=='Initialize'){
                     echo "PATH = ${M2_HOME}/bin:${PATH}"
                 echo "M2_HOME = /opt/maven"
+	  sh 'git clone https://github.com/mtomar247/SharedLibrary.git'
   }
 
  else if("$stageName"=='Maven Build Test Install') {
