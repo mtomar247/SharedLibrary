89ef06af00168cfa5c8ddd0f1c0fd2f324af723f
mtomar247 <124338224+mtomar247@users.noreply.github.com>
2023-02-27 16:25:14 +0530

    Update lib.groovy

--- vars/lib.groovy
+++ vars/lib.groovy
@@ -1,1 +1,16 @@
+def call(String stageName){
+  if ("$stageName"=='Initialize'){
+                    echo "PATH = ${M2_HOME}/bin:${PATH}"
+                echo "M2_HOME = /opt/maven"
+  }
+
+ else if("$stageName"=='Maven Build Test Install') {
+
+    sh 'mvn -B -DskipTests clean package'
+		sh 'mvn test'
+		sh 'mvn -B -DskipTests clean install'
+                }
+            }
+       }
+}
 
