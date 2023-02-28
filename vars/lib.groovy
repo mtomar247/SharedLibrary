1b8cfef3a7093c7838249829d13312262e35031c
mtomar247 <124338224+mtomar247@users.noreply.github.com>
2023-02-28 14:09:04 +0530

    Update lib.groovy

--- vars/lib.groovy
+++ vars/lib.groovy
@@ -1,17 +1,30 @@
 def call(String stageName){
   if ("$stageName"=='Initialize'){
-                    echo "PATH = ${M2_HOME}/bin:${PATH}"
-                echo "M2_HOME = /opt/maven"
-	  sh 'git clone https://github.com/mtomar247/SharedLibrary.git'
+          echo "PATH = ${M2_HOME}/bin:${PATH}"
+          echo "M2_HOME = /opt/maven"
+	  echo "This is $stageName stage."
   }
 
  else if("$stageName"=='Maven Build Test Install') {
+	echo "This is $stageName stage."
+	sh 'mvn -B -DskipTests clean package'
+	sh 'mvn test'
+	sh 'mvn -B -DskipTests clean install'
+       }
+ else if("$stageName"=='Build Docker Image') {
+	echo "This is $stageName stage.Image Build is in progress...."
+	sh 'docker build -t springboottest-docker-image .'
+	echo "Docker build Image is success." 
+       }      
+ else if("$stageName"=='Logging into AWS ECR') {
+	echo "This is $stageName stage.Image Build is in progress...."
+	sh 'docker build -t springboottest-docker-image .'
+	echo "Docker build Image is success." 
+       }    
 
-    sh 'mvn -B -DskipTests clean package'
-		sh 'mvn test'
-		sh 'mvn -B -DskipTests clean install'
-                }
-            }
+
+
+}
        
 
 
